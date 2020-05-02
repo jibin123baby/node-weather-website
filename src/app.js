822d70a220a0c86ee9410fed20691617edf5cc2d
jibin123baby <jibin123baby@gmail.com>
2020-05-02 14:14:30 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+var hbs = require('hbs');
+const geoCoding = require('./utils/geoCode');
+const forecast = require('./utils/forecast');
+
+// Define Paths for Express Config
+const pubicDirectoryPath =path.join(__dirname,'../public/');
+const viewsPath = path.join(__dirname,"../templates/views");
+const partialsPath = path.join(__dirname,"../templates/partials");
+
+//Setting Static directory to serve
+app.use(express.static(pubicDirectoryPath,{
+    extensions: ["html"],
+    })
+  );
+//ssetting handlebars engine and views location 
+app.set('view engine', 'hbs');  // To render hbs files
+app.set('views',viewsPath); //We are changing the default path from views to templates
+hbs.registerPartials(partialsPath);
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title: "Weather App",
+        name:"Jibin Baby"
+    });
+});
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title: "About",
+        name:"Jibin Baby"
+    });
+});
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        help: "We are very happy to help you. Thanks",
+        title: "Help",
+        name:"Jibin Baby"
+    });
+});
+
+app.get('/weather',(req,res)=>{
+    const address= req.query.address;
+    if(!address){
+        return res.send({
+            error : "Error ! Please Provide an Address"
+        });
+    }    
+    geoCoding(address,(error,{latitude,longitude,placeName} ={}) =>{
+        if(error){
+            return res.send({
+                address: address,
+                error : error                
+            });
+        }
+        else{
+            forecast(latitude,longitude,(error,forecastData)=>{
+                if(error){
+                    return res.send({
+                        address: address,
+                        error : error
+                    });
+                }else{
+                    res.send({
+                        address: address,
+                        location: placeName,
+                        forecast: forecastData.weather_descriptions + ". Temperature is " + forecastData.temperature +". But it feels like "+forecastData.feelslike
+                    });
+                }
+
+            });
+        }
+    });    
+});
+
+app.get('/product',(req,res)=>{
+    if(!req.query.search){
+        return res.send({
+            error : "Error ! Please Provide a product name"
+        });
+    }
+    res.send({
+        products:req.query.search
+    });
+
+});
+
+app.get('/help/*',(req,res)=>{
+    res.render('errorPage',{
+        title:"404",
+        name: "Jibin Baby",
+        errorMsg: "Page Not Found"
+    })
+});
+//This should be in last
+app.get('*',(req,res)=>{
+    res.render('errorPage',{
+        title:"404",
+        name: "Jibin Baby",
+        errorMsg: "Page Not Found"
+    })
+});
+
+
+app.listen(3000,()=>{
+    console.log("App is started on port 3000");
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,27 @@
+const request = require('request');
+
+const forecast = (latitude,longitude,callback) =>{
+    const url = "http://api.weatherstack.com/current?access_key=10bb80748b5a8d311d4e181b17a0506c&query="+encodeURIComponent(latitude)+","+encodeURIComponent(longitude);
+   // const url = "http://api.weatherstack.com/current?access_key=10bb80748b5a8d311d4e181b17a0506c&query=&units=f";
+    //request({url,json: true},(error,response)=>{  //before destructuring refer playgroung
+    request({url,json: true},(error,{body})=>{
+        if(error){
+            callback("Unable to Connect to Weather Services",undefined)
+        }else if(body.error){
+            callback("Unable to find the weather for this location",undefined);
+        }else{
+            try {
+                callback(undefined,{
+                    weather_descriptions: body.current.weather_descriptions[0],
+                    temperature: body.current.temperature,
+                    feelslike: body.current.feelslike
+                });
+            } catch (error) {
+                callback("Error in Reading the Properties",undefined);
+            }
+            
+        }
+    });
+}
+
+module.exports=forecast;
